2a399d9ef3988de8b22673fc6de2ef7a818d0076
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-05-03 16:22:38 -0300

    Changed List Images

---
+++ assets/export.py
@@ -0,0 +1,2 @@
+first_attacker_images_list = ['./assets/atacante1/walk1.png', './assets/atacante1/walk2.png', './assets/atacante1/walk3.png', './assets/atacante1/walk4.png']
+second_attacker_images_list = ['./assets/atacante2/virus1.png', './assets/atacante2/virus2.png', './assets/atacante2/virus3.png']
--- classes/atacante/pessoaContagiada.py
+++ classes/atacante/pessoaContagiada.py
@@ -1,8 +1,9 @@
 from classes.atacante.atacante import Atacante
+from assets.export import first_attacker_images_list
 
 class PessoaContagiada(Atacante):
     def __init__(self, positionx, positiony, velx, vely):
-        list_image = ['./assets/atacante1/walk1.png', './assets/atacante1/walk2.png', './assets/atacante1/walk3.png', './assets/atacante1/walk4.png']
+        list_image = first_attacker_images_list
         resistence = 0.8 # 0 < resistence < 1
         power = 0.8 # 0 < power < 1
         super().__init__(positionx, positiony, velx, vely, resistence, power, list_image)
