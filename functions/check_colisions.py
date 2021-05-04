3664ddbced055708bbd363f5c0b2d68dbadebb24
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-05-04 01:19:05 -0300

    Refactored code

--- functions/check_colisions.py
+++ functions/check_colisions.py
@@ -1,17 +1,30 @@
 import pygame
 from pygame.locals import *
 
-def check_attacker_colisions(atacante_group, defensor_group, groups):
+def get_groups(groups):
+    return groups[0], groups[1], groups[2]
+
+def check_colisions(groups):
+    check_attacker_colisions(groups)
+    check_defensor_power_colision(groups)
+
+def check_attacker_colisions(groups):
+    atacante_group, defensor_group, defensor_power_group = get_groups(groups)
+
     attacker_colisions = pygame.sprite.groupcollide(atacante_group, defensor_group, False, False)
 
     if attacker_colisions:
         for attacker, defensores in attacker_colisions.items():
             attacker.attacker_stop()
-            defensores[0].looseLife(attacker.power, groups)
+            for defensor in defensores:
+                defensor.looseLife(attacker.power, groups)
 
-def check_defensor_power_colision(defensor_power_group, atacante_group, groups):
+def check_defensor_power_colision(groups):
+    atacante_group, defensor_group, defensor_power_group = get_groups(groups)
+
     power_colisions = pygame.sprite.groupcollide(defensor_power_group, atacante_group, True, False)
 
     if power_colisions:
         for power, attackers in power_colisions.items():
-            attackers[0].looseLife(power.power, groups)
+            for attacker in attackers:
+                attacker.looseLife(power.power, groups)
--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 import pygame
 from pygame.locals import *
 from constants import BACKGROUND, screen
-from functions.export import update_group, load_defenders, load_attackers, check_attacker_colisions, check_defensor_power_colision, load_player
+from functions.export import update_groups, load_defenders, load_attackers, check_colisions, load_player
 from intro import intro
-
 from pygame import font
-
 from menu_compra.buttons import Button_medico
-
 
 pygame.init()
 
 atacante_group = pygame.sprite.Group()
 load_attackers(atacante_group)
 
+defensor_group = pygame.sprite.Group()
+player = load_player()
 
 defensor_group = pygame.sprite.Group()
-player=load_player()
-
-defensor_group = pygame.sprite.Group()
-
 
 size = (140,72)
 defensor_power_group = pygame.sprite.Group()
 button_medico = Button_medico(0, 224, size)
 buybuttons = [button_medico]
-
 
 clock = pygame.time.Clock()
 
@@ -42,19 +36,14 @@ while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-        else:
-            player.verify_event(event,buybuttons,defensor_group, defensor_power_group,atacante_group,screen)
+        player.verify_event(event,buybuttons,defensor_group, defensor_power_group,atacante_group,screen)
 
-    check_attacker_colisions(atacante_group, defensor_group, groups)
-    check_defensor_power_colision(defensor_power_group, atacante_group, groups)
+    check_colisions(groups)
 
     screen.blit(BACKGROUND, (0, 0))
 
-    for group in groups:
-        update_group(group, screen)
+    update_groups(groups, screen)
           
     player.drawn_vaccines_score(screen)
     player.update_time()
-
-        
     pygame.display.update()
